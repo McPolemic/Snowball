ab79b6bab581947b9bd31328474467bb966cef6a
Joshua Bangel <falornan@gmail.com>
2013-01-19 15:28:32 -0600

    Moved tastypie urls to snowball/urls.py - GetItWorkingForNow(tm)

--- calc/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.conf.urls.defaults import *
-from calc.api import AccountResource
-
-account_resource = AccountResource()
-
-urlpatterns = patterns( '',
-	(r'^api/', include( account_resource.urls )),
-)
