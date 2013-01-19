87b6e39c16bb4d1ee3c09f91a988e09be7a88d15
Joshua Bangel <falornan@gmail.com>
2013-01-19 12:35:37 -0600

    Added collections

--- calc/static/js/calc.js
+++ calc/static/js/calc.js
@@ -1,31 +1,37 @@
 
 var Account = Backbone.Model.extend({
-	urlRoot: '/accounts',
-
 	defaults: {
 		name: 'Default name',
 		initial_balance: 0.00,
+		current_balance: 0.00,
 		interest_rate: 0.00,
 		overpayments: true,
 		minimum_payment: 0.00
 	}
 });
 
-// Need to create an account list collection
+var AccountList = Backbone.Collection.extend({
+	model: Account,
+	url: '/accounts'
+});
 
 var Transaction = Backbone.Model.extend({
-	urlRoot: '/transactions',
-
 	defaults: {
 		account_id: 0,
-		account_name: 'Default name',
 		date: '01/01/0101',
 		amount: 0.00
 	}
 });
 
-// Need to create a transaction list collection
+var TransactionList = Backbone.Collection.extend({
+	model: Transaction,
+	url: '/transactions'
+});
 
+var AccountListView = Backbone.View.extend({
+	tagName: 'div',
+	id: 'accountList'
+});
 // Need to create views for everything
 
 var CalcApp = Backbone.Router.extend({
@@ -36,11 +42,7 @@ var CalcApp = Backbone.Router.extend({
 
 	initialize: function(){
 		// This needs to be pointing at an account list, not a specific account
-		this.account = new Account();
-
-		// This shouldn't even be here, but until we have an account list
-		// it won't make a difference
-		this.transaction = new Transaction({ model: account });
+		this.accountList = new AccountList();
 	},
 
 	start: function(){
