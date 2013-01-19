b9e2d074f2f4420555d7eba2cc429841d99806dd
Adam Lukens <spawn968@gmail.com>
2013-01-19 16:36:44 -0500

    Added users to Account,Transaction. Imported first run at fixtures

--- calc/models.py
+++ calc/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Account(models.Model):
+    user = models.ForeignKey(User)
     name = models.CharField(max_length=50, unique=True)
     initial_balance = models.DecimalField(max_digits=9, decimal_places=2)
     interest_rate = models.DecimalField(max_digits=5, decimal_places=3)
@@ -8,9 +10,14 @@ class Account(models.Model):
     minimum_payment = models.DecimalField(max_digits=9, decimal_places=2)
 
     def __unicode__(self):
-        return self.name
+<<<<<<< Updated upstream
+        return '%s - %s' % (self.user.username, self.name)
+=======
+        return '%s - %s' % (self.user.name, self.name)
+>>>>>>> Stashed changes
 
 class Transaction(models.Model):
+    user = models.ForeignKey(User)
     account = models.ForeignKey(Account)
     date = models.DateField()
     amount = models.DecimalField(max_digits=9, decimal_places=2)
