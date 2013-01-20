52620e42106d19b860490ac8c7d4aff73361f284
Joshua Bangel <falornan@gmail.com>
2013-01-20 17:43:34 -0600

    Updated urls to handle api URIs.  This allows for schema lookups

--- snowball/urls.py
+++ snowball/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.defaults import *
-from calc.api import AccountResource
+
+from tastypie.api import Api
+from calc.api import AccountResource, TransactionResource
 
 import calc.views
 
@@ -8,7 +10,9 @@ import calc.views
 from django.contrib import admin
 admin.autodiscover()
 
-account_resource = AccountResource()
+v1_api=Api(api_name='v1')
+v1_api.register(AccountResource())
+v1_api.register(TransactionResource())
 
 urlpatterns = patterns('',
     # Examples:
@@ -21,7 +25,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^api/', include(account_resource.urls)),
+    url(r'^api/', include(v1_api.urls)),
 
     url(r'^/*$', calc.views.calc_main_view),
 )
